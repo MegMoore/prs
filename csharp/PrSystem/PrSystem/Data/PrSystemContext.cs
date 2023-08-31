3087edc613380e674312b3b607f313d5a6f2b70d
Megan Moore <meganmoore.dev@gmail.com>
2023-08-31 10:25:01 -0400

    update

---
+++ csharp/PrSystem/PrSystem/Data/PrSystemContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PrSystem.Models;
+
+namespace PrSystem.Data
+{
+    public class PrSystemContext : DbContext
+    {
+        public PrSystemContext (DbContextOptions<PrSystemContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<PrSystem.Models.User> Users { get; set; } = default!;
+    }
+}
---
+++ csharp/PrSystem/PrSystem/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PrSystem.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<PrSystemContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("PrSystemContext") ?? throw new InvalidOperationException("Connection string 'PrSystemContext' not found.")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
